f91e8aae47513fba2cf6cdd890abfcd354bfc36c
Daria Kharkovskaya <dicsi59@gmail.com>
2025-04-16 22:16:59 +0300

    исправления в стилях

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -6,5 +6,6 @@ import ru.practicum.shareit.item.model.Comment;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findAllByItemId (long id);
+    
+    List<Comment> findAllByItemId(long id);
 }
