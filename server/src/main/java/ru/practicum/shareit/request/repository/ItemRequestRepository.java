d84882dbe6bc2b7b694e774dd79c16c73b65dacd
Daria Kharkovskaya <dicsi59@gmail.com>
2025-04-26 15:42:29 +0300

    Исправление стилей

--- server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.request.repository;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
--- server/src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
@@ -1,13 +1,9 @@
 package ru.practicum.shareit.request.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.error.exception.NotFoundException;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.mapper.MapperItem;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
@@ -19,14 +15,12 @@ import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RequestServiceImpl implements RequestService {
     private final UserRepository userRepository;
     private final ItemRequestRepository itemRequestRepository;
-    private final ItemRepository itemRepository;
     private final ItemService itemService;
 
     @Override
@@ -47,21 +41,10 @@ public class RequestServiceImpl implements RequestService {
     @Transactional(readOnly = true)
     @Override
     public ItemRequestDto getById(Long requestId, Long requestorId) {
-        //userRepository.findById(requestorId).orElseThrow(() -> new NotFoundException(""));
-        //return itemRequestRepository.findById(requestId).orElseThrow(() -> new NotFoundException(""));
         userRepository.findById(requestorId).orElseThrow(() -> new NotFoundException(""));
         ItemRequest itemRequest = itemRequestRepository.findById(requestId).orElseThrow(() -> new NotFoundException(""));
         ItemRequestDto itemRequestDto = MapperItemRequests.toItemRequestDto(itemRequest);
-        List <ItemDto> itemDto1 = itemService.findAllByRequestId(itemRequestDto.getId());
-        List <ItemDto> itemDto2 = itemService.findAllByRequestId(requestId);
-        List <ItemDto> itemDto3 = itemService.findAllByRequestId(requestorId);
-        List <Item> itemDto4 = itemRepository.findByRequestId(requestorId);
-        List <Item> itemDto5 = itemRepository.findAllByRequestId(requestorId);
         itemRequestDto.setItems(itemService.findAllByRequestId(itemRequestDto.getId()));
         return itemRequestDto;
-
-
-
-
     }
 }
